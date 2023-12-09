db0d425e3dbb0415924c88e6f5a05efaf2ac7a93
SY-Gato <148269795+SY-Gato@users.noreply.github.com>
2023-12-08 21:00:05 -0600

    Update CreateModuleScript.lua

--- CreateModuleScript.lua
+++ CreateModuleScript.lua
@@ -1,3 +1,11 @@
 if game:GetService("HttpService").HttpEnabled == false then error("Game does not allow http requests! Cannot fetch script.") end
 local httpserv = game:GetService("HttpService")
-local website = "
+local website = "https://sy-gato.github.io/UiLib/lib.lua"
+local data
+local funcsuccess, statusres = pcall(function()
+    data = httpserv:GetAsync(website)
+    print(tostring(data))
+end)
+if funcsuccess == false then
+  print(statusres)
+end
